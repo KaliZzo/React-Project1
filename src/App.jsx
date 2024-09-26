b5e62ea2e23dd46de64f0e13b12705672d581e87
nisim <nisimohana44@gmail.com>
2024-09-26 15:51:28 +0300

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -33,23 +33,27 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
+            {CORE_CONCEPTS.map((item) => (
+              <CoreConcept key={item.title} {...item} />
+            ))}
+
+            {/* <CoreConcept {...CORE_CONCEPTS[0]} />
             {/* Example how this code could be shorter */}
-            <CoreConcept
+            {/* <CoreConcept
               title={CORE_CONCEPTS[1].title}
               description={CORE_CONCEPTS[1].description}
               image={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcept
+            /> */}
+            {/* <CoreConcept
               title={CORE_CONCEPTS[2].title}
               description={CORE_CONCEPTS[2].description}
               image={CORE_CONCEPTS[2].image}
-            />
-            <CoreConcept
+            /> */}
+            {/* <CoreConcept
               title={CORE_CONCEPTS[3].title}
               description={CORE_CONCEPTS[3].description}
               image={CORE_CONCEPTS[3].image}
-            />
+            /> */}
           </ul>
         </section>
         <section id="examples">
